44c7d8d32f638b38f6a541f0f292ee8dd6cde9ef
cvzwahlen <zwa16001@byui.edu>
2017-07-01 15:07:35 -0600

    SWAPI - still doesn't work

--- sandbox/fetch-swapi-javascript.js
+++ sandbox/fetch-swapi-javascript.js
@@ -1,41 +1,61 @@
-var data;
+function getJSON(url) {
+    return fetch(url)
+        .then(function (response){
+        return response.json();
+    })
+        .catch(function(error){
+        console.log(error);
+    });
+}
 
-var results = data.results;
-var shipListElement = document.getElementById('shiplist'); //shiplist in HTML is an empty list
-shipListElement.innerHTML = "";
+function fetchShips(){
 
-//forEach is made to loop th    rough arrays
+}
 
-results.forEach(function (ship){
-var listItem = document.createElement('a');
-link.setAttribute('href', ship.url);
-link.innerHTML = ship.name;
-listItem.appendChild(link);
-shipListElement.appendChild(listItem);
-
-
-//create elements for list...li and a
-
-//set href attribute to the URL of the ship
-
-//set the contents of the link element to the name of the ship
-
-//add the link to the list item
-
-//and the list item to the list
-
-});
 
 
 function fetchShips() {
     var url = "//swapi.co/api/starships/";
-    //call getJSON function to get the list of ships from the api
-    getJSON(url).then(function (data) {
-        //stuff that should happen after the request is done.
-        console.log(data);
+    getJSON(url).then(function (data){
+
+
+        var results = data.results;
+        var shipListElement = document.getElementById('shiplist');
+
+        shipListElement.innerHTML = "";
+
+        results.forEach(function (ship){
+
+            var listItem = document.createElement('li');
+            var listItem = document.createElement('a');
+
+            link.setAttribute('href', ship.url);
+            link.addEventListener('click', function(event){
+                event.preventDefault();
+                getShipDetails(ship.url);
+            })
+
+           link.innerHTML = ship.name;
+
+            listItem.appendChild(link);
+
+            shipListElement.appendChild(listItem);
 
     });
-}
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
 
 
 function getShipDetails(url) {
@@ -53,6 +73,9 @@ function getShipDetails(url) {
 
 
 fetchShips();
+
+
+//Lynnette, Breena, Casey
 
 //data.results[0].name or
 //data.results[0].url
