40f4cba7764a6a9d24f64783cfeb6729bda70ed0
cvzwahlen <zwa16001@byui.edu>
2017-07-13 15:38:22 -0600

    Final Project initial upload working

--- cookbook-javascript.js
+++ cookbook-javascript.js
@@ -1,55 +1,150 @@
-    function getJSON(url) {
+function getJSON(url) {
         return fetch(url)
             .then(function (response) {
-                return response.json();
-            })
+            return response.json();
+        })
             .catch(function (error) {
-                console.log(error);
-            });
+            console.log(error);
+        });
     }
 
     // Get the data from the wunderground API
-    function getData(cookbook) {
+    function getBreads() {
         var url = "cookbook.json";
         getJSON(url).then(function (data) {
             console.log(data);
-            document.getElementById("titleHead").innerHTML= "It works";
+
+          var breads = data.Breads;
 
 
-
-
-
-//            document.getElementById("titleHead").innerHTML=data.Breads.name;
-//            document.getElementById("summary").innerHTML=data.current_observation.weather;
-//            document.getElementById("currentTemp").innerHTML=data.current_observation.temp_f+ "&#8457";
-//            document.getElementById("summary").innerHTML=data.current_observation.weather;
-//            document.getElementById("add1").innerHTML="Feels like: " + data.current_observation.feelslike_f + "&#8457";
-//            document.getElementById("add2").innerHTML="Wind: " + data.current_observation.wind_string;
-//            document.getElementById("add3").innerHTML=data.current_observation.local_time_rfc822;
-
-            //add the code necessary here to update the page with all of the correct data points.
-
-            //this line will cause the Loading message to fade away.
-//            document.getElementById("cover").classList.add('fadeout');
+            var recipeListElement = document.getElementById("recipeList");
+            breads.forEach(function(recipe){
+               var recipeCall = createRecipe(recipe);
+                recipeListElement.appendChild(recipeCall);
+            });
 
         });
 
 
     };
 
+function getDesserts() {
+    var url = "cookbook.json";
+    getJSON(url).then(function (data) {
+        console.log(data);
+
+        var desserts = data.Desserts;
+
+
+        var recipeListElement = document.getElementById("recipeList");
+        desserts.forEach(function(recipe){
+            var recipeCall = createRecipe(recipe);
+            recipeListElement.appendChild(recipeCall);
+        });
+
+    });
+
+
+};
+
+function getMainDish() {
+    var url = "cookbook.json";
+    getJSON(url).then(function (data) {
+        console.log(data);
+
+        var mainDish = data.mainDish;
+
+
+        var recipeListElement = document.getElementById("recipeList");
+        mainDish.forEach(function(recipe){
+            var recipeCall = createRecipe(recipe);
+            recipeListElement.appendChild(recipeCall);
+        });
+
+    });
+
+
+};
+
+
+function getSalads() {
+    var url = "cookbook.json";
+    getJSON(url).then(function (data) {
+        console.log(data);
+
+        var salads = data.Salads;
+
+
+        var recipeListElement = document.getElementById("recipeList");
+        salads.forEach(function(recipe){
+            var recipeCall = createRecipe(recipe);
+            recipeListElement.appendChild(recipeCall);
+        });
+
+    });
+
+
+};
+
+function getSnacks() {
+    var url = "cookbook.json";
+    getJSON(url).then(function (data) {
+        console.log(data);
+
+        var snacks = data.Snacks;
+
+
+        var recipeListElement = document.getElementById("recipeList");
+        snacks.forEach(function(recipe){
+            var recipeCall = createRecipe(recipe);
+            recipeListElement.appendChild(recipeCall);
+        });
+
+    });
+
+
+};
 
 
 
 
 
-    // A function for changing a string to TitleCase
-//    function toTitleCase(str) {
-//        return str.replace(/\w+/g, function (txt) {
-//            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-//        });
-//    }
 
 
-getJSON();
-getData();
 
+
+
+function createRecipe(recipe){
+    var container = document.createElement('div');
+    var title = document.createElement('h1');
+    var img = document.createElement('img');
+    var ingredientList = document.createElement('dl');
+    var steps = document.createElement('p');
+    var cookTime = document.createElement('p');
+
+    title.innerHTML = recipe.name;
+    steps.innerHTML = recipe.steps;
+    cookTime.innerHTML = "Cook Time: " + recipe.cookTime;
+    img.setAttribute("src", recipe.imageURL);
+
+
+    recipe.ingredients.forEach(function(ing){
+        var quantity = document.createElement('dt');
+        var ingName = document.createElement('dd');
+        quantity.innerHTML = ing.quantity;
+        ingName.innerHTML = ing.name;
+        ingredientList.appendChild(quantity);
+        ingredientList.appendChild(ingName);
+    });
+
+
+
+
+    container.appendChild(title);
+
+    container.appendChild(img);
+    container.appendChild(ingredientList);
+    container.appendChild(steps);
+    container.appendChild(cookTime);
+    return container;
+}
